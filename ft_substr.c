8e22ef7478e2bdfc56df09722c4dee52a9159df0
Marialena GALLIZZI <mgallizz@e2r11p23.42.fr>
2020-12-24 07:07:17 +0100

    last check

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mgallizz <mgallizz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 14:48:46 by mgallizz          #+#    #+#             */
-/*   Updated: 2020/12/24 00:55:23 by mgallizz         ###   ########.fr       */
+/*   Updated: 2020/12/24 07:04:21 by mgallizz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,15 @@ char		*ft_substr(char const *s, unsigned int start, size_t len)
 	j = 0;
 	if (!s)
 		return (NULL);
-	if (!(str = malloc(sizeof(char) * (len + 1))))
-		return (NULL);
-	if (start < ft_strlen(s))
+	if (start >= ft_strlen(s))
 	{
+		if (!(str = malloc(sizeof(char) * 1)))
+			return (NULL);
+	}
+	else
+	{
+		if (!(str = malloc(sizeof(char) * (len + 1))))
+			return (NULL);
 		while (j < len && s[start + j])
 		{
 			str[j] = s[start + j];
